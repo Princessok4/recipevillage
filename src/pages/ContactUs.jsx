87d535cfb71db813d51e66651af46471f8cf5fd4
Abdullah Lawal <abdullah.lawal@optimusbank.com>
2025-02-24 21:33:12 +0100

    worked on the contact us page

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import Navbar from '../componenet/NavBar';
+
+const teamMembers = [
+  {
+    name: 'Aturaka Tomilola',
+    role: 'Fullstack Developer',
+    email: 'tomilolaaturaka@gmail.com',
+    phone: '08067014209',
+    image: '/images/Oluwatomilola Aturaka.jpg'
+  },
+  {
+    name: 'Mena Orona',
+    role: 'UI/UX Designer',
+    email: 'oronamena18@gmail.com',
+    phone: '07086059414',
+    image: '/images/Mena.jpg'
+  },
+  {
+    name: 'Olasupo Oyinkansola',
+    role: 'Backend Developer',
+    email: 'oyinkansolaolasupo@gmail.com',
+    phone: '08032795450',
+    image: '/images/Olasupo Oyinkansola.jpg'
+  },
+  {
+    name: 'Olukoyede Moyosore',
+    role: 'Frontend Developer',
+    email: 'OlukoyedeMoyosore@gmail.com',
+    phone: '07048240577',
+    image: '/images/Moyo.jpg'
+  },
+
+  {
+    name: 'Ebireri Temisan',
+    role: 'Frontend Developer',
+    email: 'ebireritemisan@gmail.com',
+    phone: '08163723002',
+    image: '/images/Temi.jpg'
+  },
+  
+  {
+    name: 'Anthony Apkan',
+    role: 'Backend Developer',
+    email: 'tonyakpan45@gmail.com',
+    phone: '09020339649',
+    image: '/images/Anthony.jpg'
+  },
+
+
+  {
+    name: 'Blessing Isibor',
+    role: 'PHP Developer',
+    email: 'iyobosa.isibor022@gmail.com',
+    phone: '08179661363',
+    image: '/images/blessing.jpg'
+  },
+
+  {
+    name: 'Chibuike Ogbonna',
+    role: 'Frontend Developer',
+    email: 'Davidoooc2000@gmail.com',
+    phone: '08168653251',
+    image: '/images/chibuike.jpg'
+  },
+  {
+    name: 'Taofeek Ishola',
+    role: 'Backend developer(flask, NestJs)',
+    email: 'taofeekishola1791@gmail.com',
+    phone: '08074448522',
+    image: '/images/Taofeek.jpg'
+  },
+
+  {
+    name: 'Abdulazeez Kolawole',
+    role: 'Frontend Developer',
+    email: 'kolawoleabdulazeez5@gmail.com',
+    phone: '09075004018',
+    image: '/images/Azeez.jpg'
+  },
+
+  {
+    name: 'Princess Okoroafor',
+    role: 'Frontend Developer',
+    email: 'princessoutlook@gmail.com',
+    phone: '08162223056',
+    image: '/images/princess.png'
+  },
+
+  {
+    name: 'Alaegbu Ugochukwu',
+    role: 'Backend Developer',
+    email: 'Ugooalaegbu@gmail.com',
+    phone: '08108651252',
+    image: '/images/Ugo.png'
+  },
+
+  {
+    name: 'Alaegbu Ugochukwu',
+    role: 'Backend Developer',
+    email: 'Ugooalaegbu@gmail.com',
+    phone: '08108651252',
+    image: '/images/Ugo.png'
+  },
+
+];
